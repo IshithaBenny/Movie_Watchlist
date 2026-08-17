a6229a2d7cf5742f951cc2282e26c475e9964b03
Ishitha Benny <ishithabenny@gmail.com>
2026-08-17 23:49:42 +0530

    Update axios baseURL to live Render backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api/',
+    baseURL: ' https://movie-watchlist-backend-w1sr.onrender.com',
 });
 
 // This intercepts EVERY request and glues your token to it
